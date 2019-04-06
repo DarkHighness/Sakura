a720ea15d31b41892f62a1bf4817b02940a03026
Twiliness <729294652hs@gmail.com>
2019-04-06 23:32:56 +0800

    Update

---
+++ .azure/msys2-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+cd %SRC_DIR%
+
+qmake -Wall -Wlogic -Wparser CONFIG+=release
+
+mingw32-make.exe
+
+ls release
+
+mkdir package
+
+mv release/sakura.exe package
+
+mv sarasa-mono-sc-regular.ttf package
+
+cd package
+
+windeployqt --qmldir %CD:~0,2%\msys64\mingw64\share\qt5\qml --release --no-patchqt --libdir ./libs --plugindir ./plugins sakura.exe
+
+7z -tZip a sakura.zip ./package/* -mx9
